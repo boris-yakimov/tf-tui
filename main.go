d00abdfd74f977b37812d6ef7e2550be22a31086
Boris Yakimov <boris.yakimov@itgix.com>
2024-12-24 15:05:33 +0200

    add terminal dimension updates, restructure code

--- main.go
+++ main.go
@@ -12,75 +12,8 @@ import (
 // TODO: split the init, update and veiw parts into a separate package -- only do this once we have things working already !
 // TODO: how do we test that we are already logged in to the right account - maybe a simple s3 ls
 
-// TUI model
-type model struct {
-	choices []string // stores choices that will appear in the menu
-	cursor  int      // position where we are in the menu
-
-	output string // stores command outputs
-	err    error  // stores any error that occurs
-}
-
 type errMsg error
 type doneMsg struct{}
-
-func (m model) Init() tea.Cmd {
-	// TODO: make sure we are in the right directory of the lz project before doing anything
-	// TODO: make sure we are logged in to aws account
-	// TODO: render ascii logo - maybe this should be in view() ?
-	// tfBackendPath := "backends/dev.tfbackend"
-	// return tfInit(tfBackendPath)
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	case tea.KeyMsg:
-		// keep track of which key was pressed
-		switch msg.String() {
-
-		case "q", "ctrl+c":
-			return m, tea.Quit
-
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		case "enter":
-			tfBackendPath := "backends/dev.tfbackend"
-			tfVarsPath := "vars/dev.tfvars"
-			// TODO: show full init output
-			// TODO: wait for init to finish
-			// TODO: only than proceed to plan
-			// TODO: show full output in trail mode as it appears on the screen
-
-			return m, (tfAction("plan", tfVarsPath, tfBackendPath))
-		}
-	}
-
-	// return updated model to the Bubble Tea runtime for processing
-	return m, nil
-}
-
-func (m model) View() string {
-	s := "Choose an option:\n\n"
-	for i, choice := range m.choices {
-		cursor := " " // cursor indicator
-		if i == m.cursor {
-			cursor = ">" // highlight current choice
-		}
-		s += fmt.Sprintf("%s %s\n", cursor, choice)
-	}
-	s += "\nPress q to quit.\n"
-	return s
-}
 
 func tfInit(backendPath string) tea.Cmd {
 	// TODO: error if no file exists on path
@@ -156,6 +89,87 @@ func tfAction(tfAction string, varFilePath string, tfBackendPath string) tea.Cmd
 
 		return doneMsg{}
 	}
+}
+
+// TUI model
+type model struct {
+	choices []string // stores choices that will appear in the menu
+	cursor  int      // position where we are in the menu
+
+	// terminal dimentions
+	width  int
+	height int
+
+	// output string // stores command outputs
+	// err    error  // stores any error that occurs
+}
+
+// a pointer here because we want to keep seting the right dimentions of the terminal in the model itself every time that m.Update() is called
+func (m *model) updateTerminalDimentions(width, height int) {
+	m.width = width
+	m.height = height
+}
+
+func (m model) Init() tea.Cmd {
+	// TODO: make sure we are in the right directory of the lz project before doing anything
+	// TODO: make sure we are logged in to aws account
+	// TODO: render ascii logo - maybe this should be in view() ?
+	// tfBackendPath := "backends/dev.tfbackend"
+	// return tfInit(tfBackendPath)
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// keep track of terminal dimensions
+	case tea.WindowSizeMsg:
+		m.updateTerminalDimentions(msg.Width, msg.Height)
+
+	case tea.KeyMsg:
+		// keep track of which key was pressed
+		switch msg.String() {
+
+		case "q", "ctrl+c":
+			return m, tea.Quit
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		case "enter":
+			tfBackendPath := "backends/dev.tfbackend"
+			tfVarsPath := "vars/dev.tfvars"
+			// TODO: show full init output
+			// TODO: wait for init to finish
+			// TODO: only than proceed to plan
+			// TODO: show full output in trail mode as it appears on the screen
+
+			return m, (tfAction("plan", tfVarsPath, tfBackendPath))
+		}
+	}
+
+	// return updated model to the Bubble Tea runtime for processing
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Choose an option:\n\n"
+	for i, choice := range m.choices {
+		cursor := " " // cursor indicator
+		if i == m.cursor {
+			cursor = ">" // highlight current choice
+		}
+		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	}
+	s += "\nPress q to quit.\n"
+	return s
 }
 
 func main() {
